4ab6e74788a2db81bbf436b4684e0c3ee485b238
ckubec <ckubec@gmail.com>
2023-06-14 21:52:04 -0700

    Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import logo from './rylie.jpg';
-import './App.css';
+import styled from "styled-components";
 
+const Image = styled.img`
+  width: 100%;
+`;
 
+const Header = styled.h1`
+  //Put some styles here
+  color: white;
+`;
+
+const Page = styled.div`
+  //Put some styles here
+  background-color: black;
+  height: 100%;
+  width: 100%;
+`;
+
+const Content = styled.div`
+  //Put some styles here
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Page className="App">
+        <Content>
+        <Header>Rylie Vey won the spelling bee in the 7th grade</Header>
+        <Image src={logo} className="rylie-spelling" alt="logo" />
+        </Content>
+    </Page>
   );
 }
 
